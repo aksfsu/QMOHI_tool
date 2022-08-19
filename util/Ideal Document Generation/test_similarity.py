36ee0d4dfebaa1b8194003e8946596e98607b551
simicity <simi40nm@gmail.com>
2022-08-19 12:36:09 -0700

    Update similarity test

--- util/Ideal Document Generation/test_similarity.py
+++ util/Ideal Document Generation/test_similarity.py
@@ -154,14 +154,14 @@ def calculate_similarity(word_vector, keywords, weight, doc1, doc2, doc_name1=No
     similarity = cosine_similarity([sum1], [sum2])
     return similarity[0][0]
 
-def calculate_tfidf(tfidf_obj, ideal_document_path, cache_file_path=None):
+def calculate_tfidf(tfidf_obj, ideal_document_content, cache_file_content):
     # Build TF corpus
     tf_corpus = []
-    tf_corpus.append(get_text_from_html_file(cache_file_path))
-    tf_corpus.append(get_text_from_file(ideal_document_path))
+    tf_corpus.append(cache_file_content)
+    tf_corpus.append(ideal_document_content)
     tfidf_obj.update(tf_corpus)
     # features = ctfidf.rank_tfidf(-50, print_=True)
-    features = tfidf_obj.filter_tfidf(max=0.001, print_=False)
+    features = tfidf_obj.filter_tfidf(max=0.0008, print_=False)
 
     # Export into file
     with open(join(STOPWORD_FILE_PATH, 'stopwords_tfidf.txt'), 'w') as f:
@@ -174,16 +174,9 @@ def get_label(similarity):
         return "High"
     if similarity >= 0.6:
         return "Moderate"
-    elif similarity >- 0.5:
-        return "Low"
-    return "No Similarity"
+    return "Low"
 
-def evaluate_similarity(ideal_document_path, output_file_path):
-    # Load the word vector
-    print("Loading model...")
-    word_vector = KeyedVectors.load_word2vec_format(WORD_VECTOR_PATH, binary=True)
-    print("Loaded")
-
+def evaluate_similarity(word_vector, ideal_document_path, output_file_path):
     # Precompute IDF corpus
     health_topics_path = "./health_topics_summary"
     health_topics_files = [f for f in listdir(health_topics_path) if isfile(join(health_topics_path, f))]
@@ -194,10 +187,12 @@ def evaluate_similarity(ideal_document_path, output_file_path):
     ctfidf = CustomizableTfidfVectorizer([], idf_corpus)
 
     # Create a dataframe to export the result
-    results_df = pd.DataFrame(columns=['University', 'Website', 'Similarity', 'Label'])
+    results_df = pd.DataFrame(columns=['University', 'Similarity', 'Label'])
+
+    ideal_document_content = get_text_from_file(ideal_document_path)
 
     # Run tests over the cache data of SHC websites
-    cache_dir_path = "./../../Codebase/QMOHI_input/experiments/2021/QMOHI_input_11thOct2021/output/Second Run/saved_webpages"
+    cache_dir_path = "./../../Codebase/QMOHI_input/experiments/2021/QMOHI_input_11thOct2021/output/Output 2022-08-08 16:23:21 Medical Abortion/saved_webpages"
     cache_universities = [d for d in listdir(cache_dir_path) if isdir(join(cache_dir_path, d))]
     for cache_university in cache_universities:
         # Build the path to each university's cache data
@@ -206,127 +201,69 @@ def evaluate_similarity(ideal_document_path, output_file_path):
         if not cache_files:
             continue
 
-        # Read cache files
+        # Collect cache file content
+        cache_file_content = ""
         for cache_file in cache_files:
-            # Specify the path to each cache file
-            cache_file_path = join(cache_university_path, cache_file)
-            
-            # Calculate TF-IDF
-            calculate_tfidf(ctfidf, ideal_document_path, cache_file_path)
-            
-            # Calculate similarity between the ideal document and a cached SHC website
-            similarity = calculate_similarity(word_vector, 
-                keywords=[],
-                weight=1,
-                doc1=get_text_from_file(ideal_document_path), 
-                doc2=get_text_from_html_file(cache_file_path)
-            )
-            # print(f'{cache_university}/{cache_file}: {round(similarity * 100, 3)}% ({similarity})')
+            cache_file_content += " " + get_text_from_html_file(join(cache_university_path, cache_file))
+                
+        # Calculate TF-IDF
+        calculate_tfidf(ctfidf, ideal_document_content, cache_file_content)
+        
+        # Calculate similarity between the ideal document and a cached SHC website
+        similarity = calculate_similarity(word_vector, 
+            keywords=[],
+            weight=1,
+            doc1=ideal_document_content, 
+            doc2=cache_file_content
+        )
+        # print(f'{cache_university}/{cache_file}: {round(similarity * 100, 3)}% ({similarity})')
 
-            similarity_data = {
-                'University': [cache_university],
-                'Website': [cache_file],
-                'Similarity': [similarity],
-                'Label': [get_label(similarity)]
-            }
-            
-            results_df = pd.concat([results_df, pd.DataFrame(similarity_data)], ignore_index=True)
+        similarity_data = {
+            'University': [cache_university],
+            'Similarity': [similarity],
+            'Label': [get_label(similarity)]
+        }
+        
+        results_df = pd.concat([results_df, pd.DataFrame(similarity_data)], ignore_index=True)
 
     makedirs(dirname(output_file_path), exist_ok=True)
     results_df.to_csv(output_file_path)
     # print(results_df.head())
-    return
+    return results_df
 
 
-def main():
-    # Build the search term string from commandline arguments 
-    if len(sys.argv) > 1:
-        term = " ".join(sys.argv[1:])
-        ideal_document_path = join("./output", term + ".txt")
-        output_file_path = "./similarity_" + term + ".csv"
-        evaluate_similarity(ideal_document_path, output_file_path)
-
-
-'''
 def main():
     # Load the word vector
     print("Loading model...")
     word_vector = KeyedVectors.load_word2vec_format(WORD_VECTOR_PATH, binary=True)
     print("Loaded")
 
-    # Create a dataframe for visualization
-    results_df = pd.DataFrame(columns=['Website', 'Term', 'Similarity'])
-
-    # Precompute IDF corpus
-    health_topics_path = "./health_topics_summary"
-    health_topics_files = [f for f in listdir(health_topics_path) if isfile(join(health_topics_path, f))]
-    idf_corpus = []
-    for file in health_topics_files:
-        idf_corpus.append(get_text_from_file(join(health_topics_path, file)))
-
-    ctfidf = CustomizableTfidfVectorizer([], idf_corpus)
+    # Build the search term string from commandline arguments 
+    if len(sys.argv) > 1:
+        term = " ".join(sys.argv[1:])
+        ideal_document_path = join("./output", term + ".txt")
+        output_file_path = "./similarity_" + term + ".csv"
+        evaluate_similarity(word_vector, ideal_document_path, output_file_path)
     
-    for term in EXPERIMENTAL_TERMS:
-        # Open the output file
-        output_file_path = "./similarity_experiment_term=" + term + ".csv"
-        makedirs(dirname(output_file_path), exist_ok=True)
-        output_file = open(output_file_path, 'a')
-        csv_writer = csv.writer(output_file)
-        csv_writer.writerow(['University', 'Cache File Name', 'Similarity'])
+    else:
+        results_df = pd.DataFrame(columns=['University', 'Similarity', 'Term'])
+        for term in EXPERIMENTAL_TERMS:
+            ideal_document_path = join("./output", term + ".txt")
+            output_file_path = "./similarity_" + term + ".csv"
+            result_df = evaluate_similarity(word_vector, ideal_document_path, output_file_path)
+            result_df.drop(columns='Label', inplace=True)
+            result_df['Term'] = [term] * result_df.shape[0]
+            results_df = pd.concat([results_df, result_df], ignore_index=True)
 
-        # Run tests over the cache data of SHC websites
-        cache_dir_path = "./../../Codebase/QMOHI_input/experiments/2021/QMOHI_input_11thOct2021/output/Second Run/saved_webpages"
-        cache_universities = [d for d in listdir(cache_dir_path) if isdir(join(cache_dir_path, d))]
-        for cache_university in cache_universities:
-            # Build the path to each university's cache data
-            cache_university_path = join(cache_dir_path, cache_university)
-            cache_files = [f for f in listdir(cache_university_path) if isfile(join(cache_university_path, f))]
-            if not cache_files:
-                continue
+        # Visualize the results
+        results_df = results_df.groupby(['Term'], sort=False).apply(lambda x: x.sort_values(['Similarity'], ascending=False)).reset_index(drop=True)
+        f, ax = plt.subplots(figsize=(14, 8), constrained_layout=True)
+        g = sns.lineplot(data=results_df, x='University', y='Similarity', hue='Term')
+        plt.xticks(rotation=50, horizontalalignment='right')
+        plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+        plt.title("Similarity")
+        plt.show()
 
-            # Read cache files
-            for cache_file in cache_files:
-                # Specify the path to each cache file
-                cache_file_path = join(cache_university_path, cache_file)
-                
-                # Calculate TF-IDF
-                calculate_tfidf(ctfidf, term, cache_file_path)
-                
-                # Calculate similarity between the ideal document and a cached SHC website
-                similarity = calculate_similarity(word_vector, 
-                    MEDICATED_ABORTION_KEYWORDS,
-                    3,
-                    get_text_from_file(join("./output", term + '.txt')), 
-                    get_text_from_html_file(cache_file_path),
-                    term,
-                    cache_university + "-" + cache_file
-                )
-                csv_writer.writerow([cache_university, cache_file, similarity])
-                # print(f'{cache_university}/{cache_file}: {round(similarity * 100, 3)}% ({similarity})')
-                
-                similarity_data = {
-                    'Website': [cache_university + ' ' + cache_file],
-                    'Term': [term],
-                    'Similarity': [similarity]
-                }
-                
-                results_df = pd.concat([results_df, pd.DataFrame(similarity_data)], ignore_index=True)
-
-    output_file.close()
-    # print(results_df.head())
-
-    # Visualize the results
-    results_df = results_df.groupby(['Term'], sort=False).apply(lambda x: x.sort_values(['Similarity'], ascending=False)).reset_index(drop=True)
-    results_df.reset_index(drop=True, inplace=True)
-    f, ax = plt.subplots(figsize=(14, 8), constrained_layout=True)
-    g = sns.lineplot(data=results_df, x="Website", y="Similarity", hue="Term")
-    plt.xticks(rotation=50, horizontalalignment='right')
-    plt.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
-    plt.title("Similarity")
-    plt.show()
-
-    return
-'''
 
 if __name__ == "__main__":
     main()
